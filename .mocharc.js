932184fc13330d1206fd853b3bc4d62ed40662b4
Angel X. Moreno <angelxmoreno@gmail.com>
2020-08-02 16:43:26 -0400

    improve eslint

--- .mocharc.js
+++ .mocharc.js
@@ -1,12 +1,5 @@
-/*
---require ./node_modules/ts-node/register
---require ./node_modules/source-map-support/register
---recursive
---exit
-
- */
 module.exports = {
     require: ['./node_modules/ts-node/register', './node_modules/source-map-support/register'],
-    spec: "test/**/*.spec.ts",
+    "spec": "test/**/*.spec.ts",
     recursive: true
 }
